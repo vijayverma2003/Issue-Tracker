9f4cb92bf8fb92112a2e458017819cdb1c2e102f
Vijay <vijayvermakvp@gmail.com>
2024-01-19 04:22:40 +0530

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 
 import "easymde/dist/easymde.min.css";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { createIssueSchema } from "@/app/validationSchemas";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import SimpleMDE from "react-simplemde-editor";
-import { useState } from "react";
 
-interface NewIssue {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { control, register, handleSubmit } = useForm<NewIssue>();
   const [error, setError] = useState("");
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   return (
     <div className="max-w-xl">
@@ -41,6 +48,13 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title?.message}
+          </Text>
+        )}
+
         <Controller
           name="description"
           control={control}
@@ -48,6 +62,12 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description?.message}
+          </Text>
+        )}
         <Button>Create New Issue</Button>
       </form>
     </div>
