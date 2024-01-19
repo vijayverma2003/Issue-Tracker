40a8a8ccef705746f806eb4ce2bafbb48c22827a
Vijay <vijayvermakvp@gmail.com>
2024-01-20 04:22:21 +0530

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingIssueDetail = () => {
+  return <div>Loading...</div>;
+};
+
+export default LoadingIssueDetail;
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingIssueForm = () => {
+  return <div>Loading...</div>;
+};
+
+export default LoadingIssueForm;
